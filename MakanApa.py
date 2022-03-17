bea4279439f762aaef45e1e6c7220946790e40be
galih21x <90541714+galih21x@users.noreply.github.com>
2022-03-17 23:46:30 +0700

    Delete MakanApa.py

--- MakanApa.py
+++
@@ -1,5 +0,0 @@
-hari = input("Masukkan nama hari : ")
-if (hari == "Senin" or hari == "Selasa" or hari == "Rabu" or hari == "Kamis" or hari == "Jumat" or hari == "Sabtu" or hari == "Minggu"):
-    print("Valid")
-else:
-    print("Tidak Valid")
